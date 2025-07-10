95c40ddf05b56dba4e381dc8d87a93dc3ccc2d8c
1F10240144 <s1F102401449@iniad.org>
2025-07-10 15:14:46 +0900

    コンフリクトを修正しました

--- todo/views.py
+++ todo/views.py
@@ -40,11 +40,6 @@ def update(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    task.delete()
-    return redirect(index)
-
-
-def delete(request, task_id):
     if request.method == 'POST':
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
@@ -56,3 +51,11 @@ def delete(request, task_id):
     }
     return render(request, "todo/edit.html", context)
 
+
+def delete(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    task.delete()
+    return redirect(index)
